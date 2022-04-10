27dea38ca0a3a84997db2d0fa0e492b521c9ef3c
arora <aroramadhav65@gmail.com>
2022-04-10 18:58:45 +0530

    Update

--- lib/Components/About.dart
+++ lib/Components/About.dart
@@ -62,7 +62,7 @@ class _AboutState extends State<About> {
               ),
             ],
           ),
-          const SizedBox(
+          SizedBox(
             height: 12,
           ),
           Row(
