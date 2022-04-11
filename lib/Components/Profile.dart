f1ccc90844560990431501cc4a3920cccbf04747
arora <aroramadhav65@gmail.com>
2022-04-11 20:28:16 +0530

    Update

--- lib/Components/Profile.dart
+++ lib/Components/Profile.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors
+// ignore_for_file: file_names, prefer_const_constructors_in_immutables, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:madhav_arora_portfolio/Constants/Constants.dart';
