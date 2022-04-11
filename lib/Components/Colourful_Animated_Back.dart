f172c7009f87c7055e3bdf71c03c8808b3be6a90
arora <aroramadhav65@gmail.com>
2022-04-11 20:17:32 +0530

    Update

--- lib/Components/Colourful_Animated_Back.dart
+++ lib/Components/Colourful_Animated_Back.dart
@@ -3,14 +3,14 @@
 import 'package:flutter/material.dart';
 
 class Colourful extends StatelessWidget {
-  const Colourful({Key? key}) : super(key: key);
+  Colourful({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 170,
       height: 170,
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(100)),
         gradient: LinearGradient(
           colors: [
